24f82fd96beac58aa7c125a5c31cd1eef095af4b
unknown <hugoandrevanzyl@gmail.com>
2025-03-23 16:14:39 +0000

    Week 06 Lab

---
+++ week06/lab6.1-quiz.py
@@ -0,0 +1,15 @@
+# function yo takes one parameter
+def yo(a):
+ return a * 2
+# here we are calling the function yo
+# and are passing in an argument, 3
+yo(3)
+
+'''
+a. What will be printed out when this program is run? 
+    Answer - Nothing as we dont call any print or display function
+
+b. What would you have to add to the program to get it to print out what the calling the function yo(3) returns?
+    Answer - print(yo(3))
+
+'''
